ebf69df5d3d0fd70238fb91c473fd234f937a26e
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2021-09-28 21:42:08 +0100

    commit

--- drf_todo/settings.py
+++ drf_todo/settings.py
@@ -125,9 +125,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS =[
-    os.path.join(BASE_DIR, 'todo-frontend/build/static')
-]
+# STATICFILES_DIRS =[
+#     os.path.join(BASE_DIR, 'todo-frontend/build/static')
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
