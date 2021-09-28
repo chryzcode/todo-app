77c5285a3d8016872e7f362b3c7cd6d8c8886d76
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2021-09-28 22:15:59 +0100

    commit

--- drf_todo/settings.py
+++ drf_todo/settings.py
@@ -150,6 +150,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 django_heroku.settings(locals())
 
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
-]
+# CORS_ORIGIN_WHITELIST = [
+#     'http://localhost:3000',
+# ]
