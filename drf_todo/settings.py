6d3c8ee77e1f69f67f1f808d69724d83b80be0ad
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2021-09-28 13:48:34 +0100

    commit

--- drf_todo/settings.py
+++ drf_todo/settings.py
@@ -127,10 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS =[
-    os.path.join(BASE_DIR, 'todo-frontend/build/static')
+    os.path.join(BASE_DIR, 'todo-frontend/build')
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
