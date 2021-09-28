3224b16146404f75fc392c9028cbddb524e5cba2
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2021-09-28 21:47:23 +0100

    commit

--- drf_todo/settings.py
+++ drf_todo/settings.py
@@ -60,7 +60,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'todo-frontend/build')
+            os.path.join(BASE_DIR, 'build')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
